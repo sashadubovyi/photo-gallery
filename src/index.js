e2a3ffee7b0c986ad7b2a6b097077886cc1171f6
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-11-21 14:14:40 +0200

    ss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import { App } from 'components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 
+//
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/">
